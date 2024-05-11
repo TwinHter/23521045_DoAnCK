35b77312d940a8f47d8cd543eaa2bea8973421e0
TwinHter <nhdnguyen05@gmail.com>
2024-05-12 01:45:35 +0700

    .

---
+++ main.py
@@ -0,0 +1,37 @@
+import itertools
+import string
+import time
+from zipfile import ZipFile
+
+str_zipFile = 'example.zip';
+# Define all characters to use in the password
+chars = "0123456789"
+
+# Define the password to be cracked
+
+# Define the maximum password length to try
+max_length = 6
+
+# Track the start time of the password cracking process
+start_time = time.time()
+
+# Try all possible combinations of characters up to max_length
+for length in range(1, max_length + 1):
+    for combination in itertools.product(chars, repeat=length):
+        # Join the characters in the combination to form a password candidate
+        str_pwd = "".join(combination)
+        # Check if the candidate matches the password
+        try:
+            with ZipFile(str_zipFile) as zipObj:
+                zipObj.extractall(pwd=bytes(str_pwd, 'utf-8'))
+                end_time = time.time()
+                print("Password found:", str_pwd)
+                # Calculate the time taken to crack the password
+                time_taken = end_time - start_time
+                print("Time taken:", time_taken, "seconds")
+                # Terminate the password cracking process
+                raise SystemExit
+        except RuntimeError as e:
+            continue
+    
+print("Not found password")
