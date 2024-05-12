bb77eb8d01700207efc459b2c2a27a308ae0a205
TwinHter <nhdnguyen05@gmail.com>
2024-05-12 08:52:26 +0700

    .

--- main.py
+++ main.py
@@ -2,36 +2,54 @@ import itertools
 import string
 import time
 from zipfile import ZipFile
+import json
 
-str_zipFile = 'example.zip';
-# Define all characters to use in the password
-chars = "0123456789"
+data = {}
+def get_data(state, title, content, timee):
+    # Trả về dữ liệu JSON
+    if state:
+        data_to_share = {
+            'title': title,
+            'content': content,
+            'timee': timee
+        }
+    else:
+        data_to_share = {
+            'title': 'Failed to Cracking',
+            'content': '',
+            'timee': ''
+        }
+    return data_to_share
 
-# Define the password to be cracked
+def start_cracking():
+    global data
+    str_zipFile = 'example.zip'
+    chars = string.digits  # Sử dụng tất cả các số từ 0 đến 9
+    max_length = 6
+    start_time = time.time()
 
-# Define the maximum password length to try
-max_length = 6
+    is_find = False
+    for length in range(1, max_length + 1):
+        if is_find == True:
+            break
+        for combination in itertools.product(chars, repeat=length):
+            if is_find == True:
+                break
+            str_pwd = "".join(combination)
 
-# Track the start time of the password cracking process
-start_time = time.time()
+            try:
+                with ZipFile(str_zipFile) as zipObj:
+                    zipObj.extractall(pwd=bytes(str_pwd, 'utf-8'))
+                    end_time = time.time()
+                    time_taken = int((end_time - start_time) * 1000)
+                    data = get_data(1, "Cracking Successfully", f"Password found: {str_pwd}", f"Time taken: {time_taken} miliseconds")
+                    is_find = True
+            except RuntimeError as e:
+                continue
 
-# Try all possible combinations of characters up to max_length
-for length in range(1, max_length + 1):
-    for combination in itertools.product(chars, repeat=length):
-        # Join the characters in the combination to form a password candidate
-        str_pwd = "".join(combination)
-        # Check if the candidate matches the password
-        try:
-            with ZipFile(str_zipFile) as zipObj:
-                zipObj.extractall(pwd=bytes(str_pwd, 'utf-8'))
-                end_time = time.time()
-                print("Password found:", str_pwd)
-                # Calculate the time taken to crack the password
-                time_taken = end_time - start_time
-                print("Time taken:", time_taken, "seconds")
-                # Terminate the password cracking process
-                raise SystemExit
-        except RuntimeError as e:
-            continue
-    
-print("Not found password")
+    if not is_find:
+        data = get_data(0, '', '', '')
+
+start_cracking()
+with open('data.json', 'w') as json_file:
+    json.dump(data, json_file)
